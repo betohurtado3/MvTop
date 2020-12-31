391f3fdb0595ed9f4bf9538ec141e95cfadff6ae
Beto <betohurtado3@hotmail.com>
2020-12-31 15:55:14 -0600

    Implementacion de pequeñas CARDS para las notas de comentarios en los juegos

--- Front/juegos.php
+++ Front/juegos.php
@@ -25,64 +25,66 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                            <a class="nav-link" aria-current="page" href="inicio.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="juegos.php">Juegos</a>
+                            <a class="nav-link active" href="juegos.php">Juegos</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="peliculas.php">Peliculas</a>
+                            <a class="nav-link" href="peliculas.php">Peliculas</a>
                         </li>
                     </ul>
                 </div>
             </div>
             <ul class="navbar-nav navbar-right">
-                        <li class="nav-item-end navbar-right">
-                            <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">
-                                <?php
-                                    if (isset($_SESSION['user'])) 
-                                    {                               
-                                        echo "".$_SESSION['user'];
-                                    }
-                                ?> 
+                <li class="nav-item-end navbar-right">
+                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">
+                        <?php
+                            if (isset($_SESSION['user'])) 
+                            {                               
+                                echo "".$_SESSION['user'];
+                            }
+                            ?> 
                             </a>
-                        </li>
-                        <li class="nav-item navbar-right">
-                            <a class="nav-link"  href="../Back/cerrarsesion.php">Salir</a>
-                        </li>
-                    </ul>
-
+                </li>
+                <li class="nav-item navbar-right">
+                    <a class="nav-link"  href="../Back/cerrarsesion.php">Salir</a>
+                </li>
+            </ul>
         </nav>
-    <br>
-           
-
-            
     <?php
         }
         else{
              header('Location: ../index.php'); 
             }
-        ?>
-
-
-    <?php
+        ?>   
+        <br>
+    
+        <?php
         require "../Back/conexion.php";
-        $sql="SELECT * From Noticias Where eliminado = 0";
+        $sql="SELECT * From Juegos Where eliminado = 0";
         $res = mysqli_query($con, $sql);
         $num = mysqli_num_rows($res); ///Numero de registros
          for($i = $num; $objeto = $res->fetch_object();$i++)
         {
     ?>
-        <div class="card border-dark mx-auto" style="max-width: 18rem;">
+        <div class="card border-primary mx-auto" style="max-width: 18rem;">
             <div class="card-header"><?= $objeto->Titulo?></div>
                 <div class="card-body">
-                <p class="card-text"><?=$objeto->Contenido?></p>
+                    <h4><?=$objeto->Juego?></h4>
+                    <p><?=$objeto->Descripcion?></p>
+                    <p><?=$objeto->Autor?></p>
+                    <h6 class="card-subtitle mb-2 text-muted"><?=$objeto->Fecha?></h6>
+                </div>
+            <div class="card-footer input-group mb-3">
+                <input type="text" class="form-control" placeholder="Comenta algo..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Escribir</button>
             </div>
         </div>
         <br>
         <?php
         }
         ?>
-    
+
     </body>
 </html>

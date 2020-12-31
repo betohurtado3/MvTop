ae947b729223d4a15c95a286e12f9ab00a143a3e
Leslie Nuñez <leslie-nohemy@hotail.com>
2020-12-31 14:36:16 -0600

    Se agrega la pagina de peliculas

---
+++ Front/peliculas.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+     
+    <link rel="stylesheet" href="../css/styles.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">MVTOP</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Juegos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Peliculas</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item-end">
+                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Inicar Sesion</a>
+            </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <center>
+    <img src="../img/coraline.jpg" class="img-pri" alt="coraline"></center>    
+</body>
+</html>
